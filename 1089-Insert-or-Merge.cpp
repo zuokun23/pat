955fb52f3f29dec76f80bf59962c4b2353cf5e1f
zuokun23 <1291136919@qq.com>
2018-08-20 16:47:13 +0800

    Update 1089-Insert-or-Merge.cpp

--- 1089-Insert-or-Merge.cpp
+++ 1089-Insert-or-Merge.cpp
@@ -1,3 +1,4 @@
+//法一：用数组
 #include<cstdio>
 #include<algorithm>
 
@@ -77,6 +78,81 @@ int main(){
 			tmp[i] = origin[i];
 		}
 		mergeSort();
+	}
+
+	return 0;
+}
+
+法二：用vector，并且对mergeSort加入了自己的见解
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+const int MAXN = 110;
+int n;
+
+void showArray(vector<int> &A){
+	
+	for(int i = 0 ; i < n ; i++){
+		printf("%d",A[i]);
+		if(i != n-1)
+			printf(" ");
+	}
+}
+
+bool insertSort(vector<int> &A , vector<int> &B){
+	
+	bool flag = false;
+	for(int i = 1 ; i < n ; i++){
+		int t = A[i] , j = i-1;
+		if(i != 1 && A == B)//经过一趟后查看
+			flag = true;
+		while(j >= 0 && A[j] > t){
+			A[j+1] = A[j];
+			j--;
+		}
+		A[j+1] = t;
+		if(flag == true)
+			return true;
+	}
+	return false;
+}
+
+void mergeSort(vector<int> &A ,vector<int> &B){
+	bool flag = false;
+	for(int i = 2 ; i/2 < n ; i*=2){//不用等于代表右边至少一个
+		if(i != 2 && A==B)
+			flag = true;
+		for(int j = 0 ; j + i/2 < n ; j += i){//j+i/2代表的是右边第一个元素，表示右边至少存在一个元素
+			sort(A.begin() + j, A.begin() + min(n,j+i));
+		}
+		if(flag == true){
+			showArray(A);
+			return;
+		}
+	}
+}
+int main(){
+
+	scanf("%d",&n);
+	vector<int> origin(n),tmp(n),target(n);
+	for(int i = 0 ; i < n ; i++){
+	
+		scanf("%d",&origin[i]);
+		tmp[i] = origin[i];
+	}
+	for(int i = 0 ; i < n ; i++)
+		scanf("%d",&target[i]);
+
+	if(insertSort(tmp , target)){
+		printf("Insertion Sort\n");
+		showArray(tmp);
+	}else{
+		printf("Merge Sort\n");
+		tmp = origin;
+		mergeSort(tmp,target);
 	}
 
 	return 0;
