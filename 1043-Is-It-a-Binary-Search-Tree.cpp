bf8670f164d4a5d7186b46c677178ddb92de3b09
zuokun23 <1291136919@qq.com>
2018-08-29 20:54:59 +0800

    Update 1043-Is-It-a-Binary-Search-Tree.cpp

--- 1043-Is-It-a-Binary-Search-Tree.cpp
+++ 1043-Is-It-a-Binary-Search-Tree.cpp
@@ -1,4 +1,4 @@
-//方法1：创建二叉搜索树，然后进行比较
+//方法1：创建二叉搜索树，然后进行比较，看起来此方法比方法2麻烦，但此方法很多函数代码是相似的，思路不复杂
 #include<cstdio>
 #include<vector>
 
@@ -87,4 +87,52 @@ int main(){
 	}else
 		printf("NO\n");
 	return 0;
+}
+
+//方法2
+/*分析：假设它是二叉搜索树，一开始isMirror为FALSE，
+根据二叉搜索树的性质将已知的前序转换为后序，转换过程中，
+如果发现最后输出的后序数组长度不为n，那就设isMirror为true，
+然后清空后序数组，重新再转换一次（根据镜面二叉搜索树的性质），
+如果依旧转换后数组大小不等于n，就输出no否则输出yes*/
+#include <cstdio>
+#include <vector>
+using namespace std;
+bool isMirror;
+vector<int> pre, post;
+void getpost(int root, int tail) {
+    if(root > tail) return ;
+    int i = root + 1, j = tail;
+    if(!isMirror) {
+        while(i <= tail && pre[root] > pre[i]) i++;
+        while(j > root && pre[root] <= pre[j]) j--;
+    } else {
+        while(i <= tail && pre[root] <= pre[i]) i++;
+        while(j > root && pre[root] > pre[j]) j--;
+    }
+    if(i - j != 1) return ;
+    getpost(root + 1, j);
+    getpost(i, tail);
+    post.push_back(pre[root]);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    pre.resize(n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &pre[i]);
+    getpost(0, n - 1);
+    if(post.size() != n) {
+        isMirror = true;
+        post.clear();
+        getpost(0, n - 1);
+    }
+    if(post.size() == n) {
+        printf("YES\n%d", post[0]);
+        for(int i = 1; i < n; i++)
+            printf(" %d", post[i]);
+    } else {
+        printf("NO");
+    }
+    return 0;
 }
