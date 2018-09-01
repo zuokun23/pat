8202deef13a6fa922d9062ae25f8970ff4063bd6
zuokun23 <1291136919@qq.com>
2018-09-01 09:03:41 +0800

    Update 1045-Favorite-Color-Stripe.cpp

--- 1045-Favorite-Color-Stripe.cpp
+++ 1045-Favorite-Color-Stripe.cpp
@@ -34,3 +34,44 @@ int main(){
 	printf("%d\n",dp[m][l]);
 	return 0;
 }
+//方法二：LIS
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+
+const int maxc = 210;//最大颜色数
+const int maxn = 10010;//最大L
+int HashTable[maxc];//将喜欢的颜色--(映射)->递增序列，将不喜欢的颜色映射为-1
+int A[maxn],dp[maxn];//LIS的原数组和dp数组
+using namespace std;
+
+int main(){
+	
+	int n , m  , x;
+	scanf("%d%d",&n,&m);
+	fill(HashTable , HashTable+maxc , -1);
+	for(int i = 0 ; i < m ; i++){
+		scanf("%d",&x);
+		HashTable[x] = i;//将喜欢颜色映射为递增序列0,1,..m-1
+	}
+	int L , num = 0;//num存放颜色序列中eva喜欢的颜色总数
+	scanf("%d",&L);
+	for(int i = 0 ; i < L ;i++){
+		scanf("%d",&x);
+		if(HashTable[x] >= 0)//若是喜欢的数组，则加入A数组中
+			A[num++] = HashTable[x];
+	}
+
+	//以下为LIS问题的模板
+	int ans = -1;
+	for(int i = 0 ; i < num ; i++){
+		
+		dp[i] = 1;//边界
+		for(int j = 0 ; j < i ; j++)
+			if(A[j] <= A[i] && dp[j]+1 > dp[i])
+				dp[i] = dp[j] + 1;//状态转移方程
+		ans = max(ans , dp[i]);
+	}
+	printf("%d\n",ans);
+	return 0;
+}
