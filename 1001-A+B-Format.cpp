946b53d8232f5b5b2db19d4c2b147027200901ff
zuokun23 <1291136919@qq.com>
2018-12-27 19:19:17 +0800

    Update 1001-A+B-Format.cpp

--- 1001-A+B-Format.cpp
+++ 1001-A+B-Format.cpp
@@ -1,3 +1,22 @@
+//字符串处理，技巧
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cin >> a >> b;
+    string s = to_string(a+b);
+    int len = s.length();
+    for(int i = 0 ; i < len ; i++){
+        cout<<s[i];
+        if(s[i] == '-') continue;
+        if( (i+1)%3 == len%3 && i != len-1) printf(",");
+    }
+}
+
+//死板的数据处理
 #include<cstdio>
 #include<stack>
 using namespace std;
